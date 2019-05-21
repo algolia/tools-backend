dd5e12a37d66717609dae4668e205946e5de54a1
Maxime <maxiloc@gmail.com>
2019-05-21 14:56:22 +0200

    open beta to all algolia users

--- app/Http/Middleware/AuthenticateBeta.php
+++ app/Http/Middleware/AuthenticateBeta.php
@@ -8,6 +8,15 @@ use Closure;
 
 class AuthenticateBeta
 {
+    function endsWith($string, $endString)
+    {
+        $len = strlen($endString);
+        if ($len == 0) {
+            return true;
+        }
+        return (substr($string, -$len) === $endString);
+    }
+
     public function handle(Request $request, Closure $next)
     {
         $accessToken = $request->session()->get('accessToken');
@@ -15,22 +24,7 @@ class AuthenticateBeta
         $values = $accessToken->getValues();
         $email = $values['user']['email'];
 
-        $authorizedEmails = [
-            'maxime.locqueville@algolia.com',
-            'julien.paroche@algolia.com',
-            'michael.sokol@algolia.com',
-            'sylvain.huprelle@algolia.com',
-            'emily.hayman@algolia.com',
-            'nicolas.meuzard@algolia.com',
-            'eiji.shinohara@algolia.com',
-            'marc.helbling@algolia.com',
-            'maria.schreiber@algolia.com',
-            'sepehr.fakour@algolia.com',
-            'peter.bailey@algolia.com',
-            'julien.bourdeau@algolia.com',
-        ];
-
-        if (!in_array($email, $authorizedEmails)) {
+        if ($this->endsWith($email, '@algolia.com')) {
             return response()->json(['error' => 'not_authorized'], 403);
         }
 
